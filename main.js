ef8c0dfe512f367caf31d3e45c2c54ba3d1e5f7c
dagkanbilgin <hello@dagkanbilgin.com>
2024-06-17 01:51:56 +0300

    Enjoy.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const app = Vue.createApp({})
+const app = Vue.createApp({
+    data(){
+       return {
+        introBio: 'Sr. Performance Marketing Executive, currently working at Roipublic. My career interests are digital marketing, data analysis, e-commerce, and web design.',
+        roiURL: 'https://www.roipublic.com/'
+       };
+    },
+})
 
 app.mount('#frontend')
